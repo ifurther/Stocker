b3f23fe8ce148f8640d9a9d9570e803e6f96645b
ifurther <55025025+ifurther@users.noreply.github.com>
2025-09-29 02:53:24 +0800

    main: add ta function to get some useful info

--- Stock.py
+++ Stock.py
@@ -1,11 +1,11 @@
-import datetime
+import requests
 from io import StringIO
-import sqlite3
-import time
 import pandas as pd
 import pandas_ta as ta
-import matplotlib as mpf
-import requests
+import numpy as np
+import datetime
+import time
+import sqlite3
 
 class Stocker:
   def __init__(self, n_days, db_name):
@@ -15,7 +15,7 @@ class Stocker:
     self.source = "TWSE"
     self.rank = ""
     self.data = None
-    self.data-ta = None
+    self.data_ta = None
     self.OHLCV = None
     self.n_days = n_days
     self.db_name = db_name
@@ -73,25 +73,63 @@ class Stocker:
           date -= datetime.timedelta(days=1)
           time.sleep(sleep_time)
       #return data
-      self.data = pd.concat(data)
+      data_ = pd.concat(data)
+      data_ = self.correct_data(data=data_,get_data=True)
+      if self.data is not None:
+         newdata = [self.data, data_]
+         self.data = pd.concat(newdata).drop_duplicates()
+      else:
+         self.data = data_
+
   def get_data(self) -> pd.DataFrame:
     self.data = pd.concat(self.get_twse_stocker_date_range_date(n_days=self.n_days))
 
-  def get_ta_data(self):
-    self.data-ta = self.data[['開盤價', '最高價', '最低價', '收盤價', '成交股數']].copy()       
-    self.data-ta.columns = ['open', 'high', 'low', 'close', 'volume']
-    self.data-ta.swaplevel('交易日', '證券代號').sort_index()
+  def get_data_ta(self):
+    self.data_ta = self.data[['開盤價', '最高價', '最低價', '收盤價', '成交股數']].copy()       
+    self.data_ta.columns = ['open', 'high', 'low', 'close', 'volume']
+    self.data_ta = self.data_ta.swaplevel('交易日', '證券代號').sort_index()
 
+  def add_indicators(self, df_group):
+    df_group_ = df_group.copy()
+    df_group_.ta.sma(length=20,append=True)
+    df_group_.ta.rsi(length=14,append=True)
+    #macd = df_group_.ta.macd()
+    df_group_.ta.macd(append=True)
+    #df_group_['MACD'] = macd['MACD_12_26_9']
+    #df_group_['MACDh'] = macd['MACDh_12_26_9']
+    #df_group_['MACDs'] = macd['MACDs_12_26_9']
+    df_group_.ta.stoch(append=True)
+    #kd = df_group_.ta.stoch()
+    #df_group_['K'] = kd['STOCHk_14_3_3']
+    #df_group_['D'] = kd['STOCHd_14_3_3']
+    return df_group_ 
+
+  def cal_data(self):
+     self.data_ta = self.data_ta.groupby(level=0, group_keys=False).apply(self.add_indicators)
+     self.data_ta.columns = ['open', 'high', 'low', 'close', 'volume', 'SMA_20', 'RSI_14',
+       'MACD', 'MACDh', 'MACDs', 'K',
+       'D', 'STOCHh_14_3_3']
+  def correct_data(self, data=None, get_data=None):
+    flaot_cols = ['開盤價', '最高價', '最低價', '收盤價', '最後揭示買價', '本益比']
+    int_cols = ['成交股數',	'成交筆數',	'成交金額']
+    if data is None:
+       data = self.data
+    data.index.names=['交易日','證券代號']
+    data[flaot_cols] = data[flaot_cols].apply(lambda x: pd.to_numeric(x, errors='coerce'))
+    data[int_cols] = data[int_cols].apply(lambda x: pd.to_numeric(x, errors='coerce'))
+    data.index=data.index.set_levels(pd.to_datetime(data.index.levels[0]), level='交易日')
+    data.index=data.index.set_levels(data.index.levels[1].astype(pd.StringDtype), level='證券代號')
+    if get_data is not None:
+       return data
   def save_db(self, data, db_name) -> None:
     conn = sqlite3.connect('stocker.db')  #建立資料庫
     cursor = conn.cursor()
+    self.data = self.data.drop(['level_0','交易日'],axis=1)
     self.data.to_sql('Stocker', conn, if_exists='append', index=True)
-  
+    self.data_ta.to_sql('Stocker_ta', conn, if_exists='append', index=True)
   def load_db(self, db_nmae) -> None:
     conn = sqlite3.connect('stocker.db')  #建立資料庫
     cursor = conn.cursor()
     data = pd.read_sql('SELECT * FROM Stocker', conn, index_col=['交易日','證券代號'])
-    data.index=data.index.set_levels(pd.to_datetime(data.index.levels[0]), level='交易日')
-    data.index=data.index.set_levels(data.index.levels[1].astype(pd.StringDtype), level='證券代號')
-    data[['成交股數',	'成交筆數',	'成交金額']] = data[['成交股數',	'成交筆數',	'成交金額']].astype(pd.Int64Dtype)
+    self.correct_data()
     self.data=data
